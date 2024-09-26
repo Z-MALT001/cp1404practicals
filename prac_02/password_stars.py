edaaf0e57bd330e37a619fcbe4dc252c4bd0ce7c
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-09-27 09:57:19 +1000

    Refactor password check program to utilise functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,12 +2,25 @@
 Concealed Password
 """
 
-password = input("Enter your password (minimum 7 characters): ")
 
-while len(password) < 7:
-    print("Password must be at least 7 characters! Try again!")
+def main():
+    password = get_password()
+
+    length = len(password)
+    concealed_password = "*" * length
+    print_password(concealed_password)
+
+
+def print_password(concealed_password):
+    print(concealed_password)
+
+
+def get_password():
     password = input("Enter your password (minimum 7 characters): ")
+    while len(password) < 7:
+        print("Password must be at least 7 characters! Try again!")
+        password = input("Enter your password (minimum 7 characters): ")
+    return password
 
-length = len(password)
-concealed_password = "*" * length
-print(concealed_password)
+
+main()
