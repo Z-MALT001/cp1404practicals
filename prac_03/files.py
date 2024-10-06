7f58d1fa4d8033c0b437e084c41f1d4f93d6df5c
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-06 15:02:15 +1000

    Add files program to prac_03

---
+++ prac_03/files.py
@@ -0,0 +1,24 @@
+"""
+File reading practise
+"""
+
+FILENAME = "Wow"
+
+
+
+# with open(FILENAME, "r") as in_file:
+#     print(in_file.read()) # read() use
+#     # Will print all lines in the file
+#
+#     print(in_file.readline()) # readline() use
+#     # will print a single line from the file
+#
+#     print(in_file.readlines()) # readlines() use
+#     # will print all lines in the file within a list
+#
+#     for line in in_file: # for line in file use
+#         if line.startswith("#"):
+#             print(line)
+#         else:
+#             print("not wow")
+#     # will only print the lines from the file which start with #
