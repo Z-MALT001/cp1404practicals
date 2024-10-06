ff91dbb918f9b0d6baeedc3daeee7cdf301b8070
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-06 15:40:22 +1000

    Add questions with answers to files program

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,23 +2,43 @@
 File reading practise
 """
 
-FILENAME = "Wow"
 
+#Question 1
+# name = input("What's your name? ")
+# out_file = open("name.txt",'w')
+# print(name, file=out_file)
+# out_file.close()
 
+#Question 2
+# in_file = open("name.txt",'r')
+# name_from_file = in_file.readline().strip()
+# print(f"Hi {name_from_file}!")
+# out_file.close()
 
+#Question 3
+FILENAME = "numbers.txt"
+#
 # with open(FILENAME, "r") as in_file:
-#     print(in_file.read()) # read() use
-#     # Will print all lines in the file
+#     result = 0
+
+    # Approach 1
+    # count = 0
+    # for line in in_file:
+    #     result += int(line)
+    #     count += 1
+    #     if count == 2:
+    #         break
+    # print(result)
 #
-#     print(in_file.readline()) # readline() use
-#     # will print a single line from the file
-#
-#     print(in_file.readlines()) # readlines() use
-#     # will print all lines in the file within a list
-#
-#     for line in in_file: # for line in file use
-#         if line.startswith("#"):
-#             print(line)
-#         else:
-#             print("not wow")
-#     # will only print the lines from the file which start with #
+#     # Approach 2
+#     numbers = in_file.readlines(3)
+#     result += int(numbers[0])
+#     result += int(numbers[1])
+#     print(result)
+
+#Question 4
+with open(FILENAME, "r") as in_file:
+    result = 0
+    for line in in_file:
+        result += int(line)
+    print(result)
