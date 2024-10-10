3e72d000b94463943296f9cc8dafb8288b6df71f
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-11 09:55:14 +1000

    Refactor subject reader program to display data in strings

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,11 +7,15 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    data = []
+    data = load_data(data)
+    print()
+    for datum in data:
+        print(f"{datum[0]} is taught by {datum[1]} and has {datum[2]} students!")
+    # print(data)
 
 
-def load_data():
+def load_data(data):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
@@ -22,8 +26,11 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data += [parts]
         print("----------")
     input_file.close()
+    return data
+
 
 
 main()
