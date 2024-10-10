cc83b6512d4acc1244043808e4629e29a6de91d3
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-11 09:05:39 +1000

    Add answers to lists warmup

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -2,4 +2,17 @@
 Lists Warmup
 """
 
-
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 1. 3
+# 2. 2
+# 3. 1
+# 4. [3, 1, 4, 1, 5, 9]
+# 5. [1]
+# 6. True
+# 7. False
+# 8. False
+# 9. [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+numbers[0] = "ten"
+numbers[6] = 1
+print(numbers[2:])
+print(9 in numbers)
