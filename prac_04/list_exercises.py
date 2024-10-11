5b1dbb1b418fc7b0e6eafc055d544a492bc4f1b8
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-11 10:25:45 +1000

    Add finished code to list exercises program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,3 +2,16 @@
 List Exercises
 """
 
+# numbers_str = input("Enter 5 numbers seperated by a comma: ")
+numbers_str = "1,2,3,4,5"
+numbers_str = numbers_str.split(',')
+# print(repr(numbers_str))
+numbers = []
+for number in numbers_str:
+    numbers.append(int(number))
+    print(f"Number: {number}")
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
