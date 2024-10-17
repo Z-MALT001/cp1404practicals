da27a3aa12613b51e83a3fcde1d833e8ac403afd
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-18 09:52:23 +1000

    Add code to hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -9,4 +9,20 @@ COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Bl
                   "Amethyst": "#9966cc", "Antique White": "#fqebd7", "Apricot": "#fbceb1",
                   "Aqua": "#00ffff"}
 
+max_length = max(len(colour) for colour in COLOUR_TO_CODE.keys())
+line = max_length * '-'
+print("The available colours are: ")
+print(line)
+for colour, code in COLOUR_TO_CODE.items():
+    print(f"{colour:{max_length}}")
+print(line)
 
+
+colour_name = input("Please enter a colours name: ").lower()
+while colour_name != "":
+    try:
+        print(f"{colour_name.title()} is {COLOUR_TO_CODE[colour_name]}")
+    except KeyError:
+        print("Enter a valid colour name.")
+    colour_name = input("Please enter a colours name: ").lower()
+print("Exiting...")
