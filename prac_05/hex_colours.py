b7610349c7d21f40425c2a858b8f0abc8e8e760e
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-18 09:51:55 +1000

    Add hex colours program to prac_05

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+"""
+Hex colours lookup
+"""
+
+# http://www.color-hex.com/color-names.html
+
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff",
+                  "Alizarin Crimson": "#e32636", "Amaranth": "e52b50", "Amber": "#ffbf00",
+                  "Amethyst": "#9966cc", "Antique White": "#fqebd7", "Apricot": "#fbceb1",
+                  "Aqua": "#00ffff"}
+
+
