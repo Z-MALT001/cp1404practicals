6648a262ada8bef27234c8cfcd69ef5ede8d3be4
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-18 10:10:52 +1000

    Refactor doc string in code occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:   32 minutes
+Actual:   16 minutes
 """
 
 # get input, register strings into a dictionary, for each key in dictionary add one to the value, print
