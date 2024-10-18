32d02a22f7df666dc349cdd143424a2cedb6d94e
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-18 10:10:05 +1000

    Add code to code occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,3 +6,16 @@ Actual:   32 minutes
 
 # get input, register strings into a dictionary, for each key in dictionary add one to the value, print
 
+sentence = input("Enter a sentence: ")
+word_to_count = {}
+for string in sentence.split():
+    try:
+        word_to_count[string] += 1
+    except KeyError:
+        word_to_count[string] = 1
+
+max_length = max(len(string) for string in word_to_count.keys())
+
+# print(word_to_count)
+for string in word_to_count.keys():
+    print(f"{string:{max_length}}: {word_to_count[string]}")
