cd61ef9a28eb3025ad9a856a1b16865d4f488f5e
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-18 10:57:38 +1000

    Add start of code to email program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,4 +2,20 @@
 Get Users Email
 """
 
+# WIll get email, split email at the @, return possible name, ask if name is correct, if not
+# ask for a name from user. print email
 
+# email = input("Email: ")
+email = 'zac@gmail.com'
+email_to_name = {}
+while email != '':
+    user_information = email.split('@')
+    is_name = input(f"Is your name {user_information[0]}? (Y/N) ").upper()
+    if is_name == 'N':
+        name = input('Enter your name: ')
+        email_to_name[name] = email
+    elif is_name == 'Y':
+        email = input('Email: ')
+    else:
+        print("Invalid response...")
+    email = input('Email: ')
