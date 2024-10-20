2765ce1db0b94b68873a36f4c3204dec4cdfc1d2
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-10-20 14:54:31 +1000

    Refactor wimbledon program into functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,14 +4,21 @@ Wimbledon program
 
 import csv
 
-from Demos.RegCreateKeyTransacted import keyname
-
 FILENAME = 'wimbledon.csv'
-# Year,Country,Champion,Country,Runner-up,Score in the final
-with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-    in_file.readline()
-    in_reader = list(csv.reader(in_file))
-    # print(in_reader)
+
+
+def main():
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        in_reader = list(csv.reader(in_file))
+        # print(in_reader)
+        name_to_win, winning_countries = grab_winners_data(in_reader)
+        print("Wimbledon Champions: ")
+        display_winners(name_to_win)
+        display_winning_countries(winning_countries)
+
+
+def grab_winners_data(in_reader):
     name_to_win = {}
     winning_countries = set()
     for row in in_reader:
@@ -20,11 +27,19 @@ with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         except KeyError:
             name_to_win[row[2]] = 1
             winning_countries.add(row[1])
-    print("Wimbledon Champions: ")
+    return name_to_win, winning_countries
+
+
+def display_winners(name_to_win):
     for champ in name_to_win:
         print(f"{champ}: {name_to_win[champ]}")
+
+
+def display_winning_countries(winning_countries):
     print(f"\nThese 12 countries have won Wimbledon:"
           f"\n{', '.join(sorted(winning_countries))}")
 
-    # print(name_to_win)q
+
+# Year,Country,Champion,Country,Runner-up,Score in the final
+main()
 
