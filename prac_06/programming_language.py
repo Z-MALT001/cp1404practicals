0bbd913434386958455c468b4f427755f064e98c
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-01 09:48:02 +1000

    Add initial program to programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,1 +1,17 @@
 """Programming Language Class"""
+
+class ProgrammingLanguage:
+    def __init__(self, typing='', reflection=False, year=0):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == 'Dynamic':
+            return True
+        elif self.typing == 'Static':
+            return False
+        else:
+            return "Invalid type"
+
+
