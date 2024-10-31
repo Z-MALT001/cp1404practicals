38131ddb0705e30b256a4df5113a1b18ed8d245e
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-01 09:49:40 +1000

    Refactor __init__ function to include name in programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,7 +1,8 @@
 """Programming Language Class"""
 
 class ProgrammingLanguage:
-    def __init__(self, typing='', reflection=False, year=0):
+    def __init__(self, name='', typing='', reflection=False, year=0):
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
