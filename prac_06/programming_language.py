3864f875e1517fa2abab4c2de440450a9ebe15ed
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-01 10:02:35 +1000

    Add function doc-strings to programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,12 +2,14 @@
 
 class ProgrammingLanguage:
     def __init__(self, name='', typing='', reflection=False, year=0):
+        """Initialise ProgrammingLanguage class"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Determine if programming language is dynamic"""
         if self.typing == 'Dynamic':
             return True
         elif self.typing == 'Static':
@@ -16,5 +18,6 @@ class ProgrammingLanguage:
             return "Invalid type"
 
     def __str__(self):
+        """Display programming language components"""
         return (f"{self.name}, {self.typing} Typing, "
                 f"Reflection={self.reflection}, First appeared in {self.year}")
