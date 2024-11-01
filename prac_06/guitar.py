53ee65d26320c836b362902a866cb5994be314d4
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-01 10:44:17 +1000

    Add program to guitar.py to prac_06

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,4 +6,26 @@ Actual Time of Completion: minutes
 """
 
 
+class Guitar:
+    """Guitar Class"""
+    def __init__(self, name='', year=0, cost=0.0):
+        """Initialise Guitar Class"""
+        self.name = name
+        self.year = year
+        self.cost = cost
 
+    def __str__(self):
+        """Display guitar components"""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self, current_year):
+        """Get guitar age"""
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Determine if guitar is vintage"""
+        age = self.get_age(self.year)
+        if age > 50:
+            return True
+        else:
+            return False
