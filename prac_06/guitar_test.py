cf06019170454477e2ea97850e73e2c5cd1ffe37
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-03 14:34:11 +1000

    Remove incorrect lines

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -8,6 +8,3 @@ guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
 print(guitar)
 print(f"{guitar.name} get_age() - Expected 102. Got {guitar.get_age()}")
 print(f"{guitar.name} is_vintage() - Expected True. Got {guitar.is_vintage()}")
-
-# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
