330ff7c5df1436106696594e2fd7eb532f388a00
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-03 14:33:25 +1000

    Add code to guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,5 +1,23 @@
 """Guitars Program
 Estimated Time: 20 minutes
+Actual Time: 25 minutes
 """
 
+from prac_06.guitar import Guitar
 
+guitars = []
+name = input('Name: ')
+while name != '':
+    year = int(input('Year: '))
+    cost = float(input('Cost: $'))
+    guitars.append(Guitar(name, year, cost))
+    print(f"{name} ({year}) : ${cost} added.\n")
+    name = input('Name: ')
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+print("... snip ...")
+print("These are my guitars:")
+max_name_length = max(len(guitar.name) for guitar in guitars)
+for i, guitar in enumerate(guitars, start=1):
+    vintage_string = "(vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>{max_name_length}} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
