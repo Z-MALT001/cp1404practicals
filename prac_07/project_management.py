1b7d77d043a64619e3ec3bdae830c6b44951cf67
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-10 15:17:11 +1000

    Add menu skeleton to project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,5 +2,36 @@
 Estimated Completion Time: 1 hour
 
 """
+import datetime
 
-from prac_07.project import
+date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
+date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+print(f"That day is/was {date.strftime('%A')}")
+print(date.strftime("%d/%m/%Y"))
+
+
+FILENAME = "projects.txt"
+MENU = """(L)oad projects  
+(S)ave projects  
+(D)isplay projects  
+(F)ilter projects by date
+(A)dd new project  
+(U)pdate project
+(Q)uit"""
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    print(f"Loaded 5 projects from {FILENAME}")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+        elif choice == "S":
+        elif choice == "D":
+        elif choice == "F":
+        elif choice == "U":
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+
