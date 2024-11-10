6ed608908ed78e703d49e66db4520670aeda6d11
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-10 16:34:33 +1000

    Add display projects function to project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,11 +3,13 @@ Estimated Completion Time: 1 hour
 
 """
 import datetime
+import json
+from prac_07.project import Project
 
-date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
-date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-print(f"That day is/was {date.strftime('%A')}")
-print(date.strftime("%d/%m/%Y"))
+# date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
+# date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+# print(f"That day is/was {date.strftime('%A')}")
+# print(date.strftime("%d/%m/%Y"))
 
 
 FILENAME = "projects.txt"
@@ -19,19 +21,39 @@ MENU = """(L)oad projects
 (U)pdate project
 (Q)uit"""
 
+
 def main():
     print("Welcome to Pythonic Project Management")
-    print(f"Loaded 5 projects from {FILENAME}")
+    projects = read_from_file()
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
+            pass
         elif choice == "S":
+            pass
         elif choice == "D":
+            display_projects(projects)
         elif choice == "F":
+            pass
         elif choice == "U":
+            pass
         else:
             print("Invalid choice")
         print(MENU)
         choice = input(">>> ").upper()
+    print("Thank you for using Pythonic Project Management")
 
+
+def display_projects(projects):
+    """Display organised projects to user"""
+    print("Incomplete projects")
+    incomplete_projects = [f"\t{project}" for project in projects if not project.is_complete()]
+    print('\n'.join(incomplete_projects))
+    print("Complete projects")
+    complete_projects = [f"\t{project}" for project in projects if project.is_complete()]
+    print('\n'.join(complete_projects))
+
+
+if __name__ == '__main__':
+    main()
