db58188cf179a769e6f7ff907242e8488cd7eb31
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-17 15:46:10 +1000

    GOT IT TO WORK!!!!!

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,9 +5,7 @@ Predicted Completion: 30 minutes
 """
 
 from kivy.app import App
-from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.properties import StringProperty
 
 MILES_TO_KM = 1.609
 
@@ -15,15 +13,20 @@ MILES_TO_KM = 1.609
 class ConvertMilesKmApp(App):
 
     def build(self):
-        # Window.size = (1000, 500)
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_conversion(self, value):
+    def handle_calculation(self):
         value = self.get_valid_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
+
+    def increment_input(self, change):
+        current_value = self.get_valid_miles()
+        new_value = current_value + change
+        self.root.ids.input_miles.text = str(new_value)
+        self.handle_calculation()
 
     def get_valid_miles(self):
         try:
@@ -31,11 +34,6 @@ class ConvertMilesKmApp(App):
             return value
         except ValueError:
             return 0
-
-    def increment_input(self, change):
-        current_value = self.get_valid_miles()
-        new_value = current_value + float(change)
-        self.root.ids.input_numbers.text = str(new_value)
 
 
 ConvertMilesKmApp().run()
