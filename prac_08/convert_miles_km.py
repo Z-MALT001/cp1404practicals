6047fad99fb78ef9486a713820e77cd55c2c9919
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-17 15:49:28 +1000

    Add docstrings to convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,24 +11,29 @@ MILES_TO_KM = 1.609
 
 
 class ConvertMilesKmApp(App):
+    """ConvertMilesKmApp class"""
 
     def build(self):
+        """Build Kivy App"""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculation(self):
+        """Calculate the miles to km calc."""
         value = self.get_valid_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def increment_input(self, change):
+        """Change the value of the input miles textbox."""
         current_value = self.get_valid_miles()
         new_value = current_value + change
         self.root.ids.input_miles.text = str(new_value)
         self.handle_calculation()
 
     def get_valid_miles(self):
+        """Validate the input miles value."""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
