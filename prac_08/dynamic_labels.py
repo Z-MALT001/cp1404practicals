59326526f3f3c6744677cf17f81e45a4f78de2a9
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-17 20:01:20 +1000

    Refactor dynamic_labels.py

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,26 +9,28 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.label import Label
 
-names = ['zac', 'bob']
 
-
-class ListDivider(App):
+class DynamicLabelsApp(App):
+    """Kivy app to dynamically create labels"""
     status_text = StringProperty()
 
     def __init__(self, **kwargs):
+        """Construct main app."""
         super().__init__(**kwargs)
-        self.names = names
+        self.names = ["zac", "bob"]
 
     def build(self):
-        self.title = 'THE LIST DIVIDER'
-        self.root = Builder.load_string('dynamic_labels.kv')
+        """Build Kivy GUI"""
+        self.title = "THE LIST DIVIDER"
+        self.root = Builder.load_file('dynamic_labels.kv')
         self.create_labels()
         return self.root
 
     def create_labels(self):
+        """Create labels for each part of the list"""
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
 
 
-ListDivider().run()
+DynamicLabelsApp().run()
