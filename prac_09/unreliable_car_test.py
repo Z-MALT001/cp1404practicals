84ca4b30e22cb8f6079c9eb40d217a16acbef7d1
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-22 10:06:27 +1000

    Add UnreliableCar class test to unreliable_car_test.py.py

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -3,3 +3,10 @@ CP1404 Zac Matlby
 Unreliable Car Test
 Estimated: 20 min
 """
+from prac_09.unreliable_car import UnreliableCar
+
+my_car = UnreliableCar("Commodore", 100, 80)
+my_car.drive(40)
+print(f"{my_car}")
+my_car.drive(10)
+print(f"{my_car}")
