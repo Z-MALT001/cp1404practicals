f24d28be355096f78675888c5bed7a6216a40346
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-24 13:05:47 +1000

    Add drive and get_fare to silver_service_taxi.py

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,14 +6,27 @@ Estimated: 20 min
 
 from prac_09.taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
-    flagfall = 4.50
+    """Silver Service Taxi Class"""
+
     def __init__(self, name, fuel, fanciness):
+        """Initialise Silver Service Taxi"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+        self.flagfall = 4.50
 
     def __str__(self):
         """Return a string like a Taxi but with current fare distance."""
         # Hummer, fuel=200, odo=0, 0km on current fare, $4.92/km plus flagfall of $4.50
-        return f"{super().__str__()}, ${self.price_per_km}/km plus flagfall of ${self.flagfall}"
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
+
+    def drive(self, distance):
+        """Drive the Taxi by distance."""
+        distance_driven = super().drive(distance)
+        return distance_driven
+
+    def get_fare(self):
+        """Return the fare of the Taxi."""
+        return super().get_fare() + self.flagfall
