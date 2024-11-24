e0c07bb00857f46836472524c61a9b6a54520733
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-24 12:55:58 +1000

    Add for loop to unreliable_car_test.py

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -6,7 +6,7 @@ Estimated: 20 min
 
 from prac_09.unreliable_car import UnreliableCar
 
-for i in range(1, 99):
+for i in range(1, 10):
     my_car = UnreliableCar("Commodore", 100, 80)
     my_car.drive(40)
     print(f"{my_car}")
