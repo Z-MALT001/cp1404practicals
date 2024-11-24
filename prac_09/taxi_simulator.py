14942564d55a231b4b383ebd56d762dc3181ae3d
Z-MALT001 <zachary.maltby@my.jcu.edu.au>
2024-11-24 14:15:33 +1000

    Add starter set up to taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,3 +3,31 @@ CP1404 Zac Maltby
 Taxi Simulator
 Estimated: 45 min
 """
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = """q)uit, c)hoose taxi, d)rive"""
+
+def main():
+    """Taxi Simulator main function"""
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    bill = 0.0
+
+    print("Let's Drive!!")
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != 'q':
+        if choice == 'c':
+            pass
+        if choice == 'd':
+            pass
+        else:
+            print("Invalid choice...")
+        print(f"Bill to date: ${bill:.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+
